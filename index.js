72787a2376e8bd1fcce162b97f8b83e5589ff3ac
Irvin Cossio Chavalier <irvin373@gmail.com>
2022-07-23 22:18:38 -0400

    install libs

--- index.js
+++ index.js
@@ -1,9 +1,103 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import * as React from 'react';
+import { NavigationContainer,  } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {AppRegistry, View} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {name as appName} from './app.json';
+import TQImage from './src/components/TQImage';
+import DataBase from './src/utils/DataBase';
+import Camera from './src/screens/Camera.screen';
+import PlantScreen from './src/screens/Plants.screen';
+import PlantDetail from './src/screens/PlantDetail.screen';
+import RecipeDetail from './src/screens/RecipeDetail.screen';
+import RecipeScreen from './src/screens/Recipes.screen';
+import InformationScreen from './src/screens/Info.screen';
+import color from './src/utils/color';
 
-AppRegistry.registerComponent(appName, () => App);
+const headerStyle = {
+  headerStyle: {
+    backgroundColor: color.greenHeader,
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+    fontSize: 21
+  },
+};
+
+const HomeStack = createStackNavigator();
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen options={headerStyle} name="Plantas" component={PlantScreen} />
+      <HomeStack.Screen options={headerStyle} name="PlantDetail" component={PlantDetail} />
+    </HomeStack.Navigator>
+  );
+}
+
+const CameraStack = createStackNavigator();
+function CameraStackScreen() {
+  return (
+    <CameraStack.Navigator>
+      <HomeStack.Screen options={headerStyle} name="Camara" component={Camera} />
+    </CameraStack.Navigator>
+  );
+}
+
+const InformationStack = createStackNavigator();
+function InformationStackScreen() {
+  return (
+    <CameraStack.Navigator>
+      <HomeStack.Screen options={headerStyle} name="EL SISTEMA TUKUYPAQ" component={InformationScreen} />
+    </CameraStack.Navigator>
+  );
+}
+
+const SettingsStack = createStackNavigator();
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen options={headerStyle} name="Recetas" component={RecipeScreen} />
+      <SettingsStack.Screen options={headerStyle} name="RecipeDetails" component={RecipeDetail} />
+    </SettingsStack.Navigator>
+  );
+}
+
+function TabOption(name, icon) {
+  return ({
+    tabBarLabel: name,
+    tabBarIcon: ({ color, size }) => (<TQImage iconSize={24} name={icon} color={color} />)
+  })
+}
+
+const Tab = createBottomTabNavigator();
+class HomeApp extends React.Component {
+  state = {
+    ready: false
+  };
+  async componentDidMount() {
+    await DataBase.populateDB();
+    // const data = await DataBase.getQuery("SELECT p.name, m.name as medicalGroup FROM Plant p INNER JOIN MedicalGroup m on p.MedicalGroupId = m.id;");
+    this.setState({ready: true});
+  }
+
+  render() {
+    if (!this.state.ready) {
+      return(<View />)
+    }
+    return (
+      <NavigationContainer>
+        <Tab.Navigator tabBarOptions={{
+            activeTintColor: color.greenHeader,
+          }}>
+          <Tab.Screen options={TabOption('Plantas', 'plant')} name="Plantas" component={HomeStackScreen} />
+          <Tab.Screen options={TabOption('Camara', 'camara')} name="Camara" component={CameraStackScreen} />
+          <Tab.Screen options={TabOption('Recetas', 'tea')} name="Recetas" component={SettingsStackScreen} />
+          <Tab.Screen options={TabOption('Informacion', 'plant')} name="Informacion" component={InformationStackScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+AppRegistry.registerComponent(appName, () => HomeApp);
--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -5,22 +5,22 @@ export default StyleSheet.create({
   textHeader: {
     color: color.textPrimary,
     fontSize: 18,
-    fontFamily: 'Lato-Bold'
+    // fontFamily: 'Lato-Bold'
   },
   headerName: {
     color: color.textPrimary,
     fontSize: 24,
     padding: 8,
-    fontFamily: 'Lato-Bold'
+    // fontFamily: 'Lato-Bold'
   },
   detailedText: {
     color: color.textPrimary,
     fontSize: 16,
-    fontFamily: 'Lato-Regular'
+    // fontFamily: 'Lato-Regular'
   },
   textBtn: {
     fontSize: 18,
-    fontFamily: 'Lato-Bold',
+    // fontFamily: 'Lato-Bold',
     color: color.white,
     padding: 8
   },
@@ -40,23 +40,23 @@ export const markdonwStyles = StyleSheet.create({
   heading3: {
     fontSize: 28,
     color: color.textPrimary,
-    fontFamily: 'Lato-Bold'
+    // fontFamily: 'Lato-Bold'
   },
   strong: {
     color: color.textPrimary,
     fontSize: 16,
-    fontFamily: 'Lato-Bold' 
+    // fontFamily: 'Lato-Bold' 
   },
   paragraph: {
     color: color.textPrimary,
     fontSize: 16,
-    fontFamily: 'Lato-Regular',
+    // fontFamily: 'Lato-Regular',
     textAlign: 'justify'
   },
   list_item: {
     color: color.textPrimary,
     fontSize: 16,
-    fontFamily: 'Lato-Regular',
+    // fontFamily: 'Lato-Regular',
     textAlign: 'justify'
   },
 });
